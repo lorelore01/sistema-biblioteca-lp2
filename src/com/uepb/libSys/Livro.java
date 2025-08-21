6738fc022aa4fabb114d120fdfc0afcd4852d4a3
Lorenzo <lorenzodonato75@Gmail.com>
2025-08-21 15:03:04 -0300

    construtor, getters e setters

---
+++ src/com/uepb/libSys/Livro.java
@@ -0,0 +1,50 @@
+package com.uepb.libSys;
+
+public class Livro {
+
+    private String titulo;
+    private String autor;
+    private int anoPublicacao;
+
+
+    public Livro(String titulo, String autor, int anoPublicacao) {
+
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anoPublicacao = anoPublicacao;
+
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public int getAnoPublicacao() {
+        return anoPublicacao;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public void setAnoPublicacao(int anoPublicacao) {
+        this.anoPublicacao = anoPublicacao;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro {" +
+                "\n-> titulo='" + titulo + '\'' +
+                "\n->, autor='" + autor + '\'' +
+                "\n->, anoPublicacao=" + anoPublicacao +
+                '}' + "\n";
+    }
+}
---
+++ src/com/uepb/libSys/Main.java
@@ -0,0 +1,13 @@
+package com.uepb.libSys;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        Livro memoriasPostumas = new Livro("Memórias Póstumas de Brás Cubas", "Machado de Assis", 1881);
+
+
+
+        System.out.println(memoriasPostumas.toString());
+    }
+}
